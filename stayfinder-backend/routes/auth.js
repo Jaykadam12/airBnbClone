1068555447c50069584e8659f30f082fbf5aa2c5
Jay kadam <166234881+Jaykadam12@users.noreply.github.com>
2025-06-21 18:20:57 +0530

    Update auth.js

--- stayfinder-backend/routes/auth.js
+++ stayfinder-backend/routes/auth.js
@@ -75,7 +75,7 @@ router.post("/logout", (req, res) => {
     res.clearCookie("token", {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       path: "/",
     });
     res.json({ message: "Logged out successfully" });
