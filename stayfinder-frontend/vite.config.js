d75abafcdc2286c4b92fee3704caca5595f0db06
Jay kadam <166234881+Jaykadam12@users.noreply.github.com>
2025-06-21 12:31:07 +0530

    Update vite.config.js

--- stayfinder-frontend/vite.config.js
+++ stayfinder-frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      "/api": "http://localhost:5000",
+      "/api": "https://airbnbclone-y56h.onrender.com",
     },
   },
 });
